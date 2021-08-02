3089862e995540fb186fa23d066299f812dd0152
sekharbabu4patnam <babuthetrainer@gmail.com>
2021-08-02 07:08:56 +0530

    Crud Operations Demo

---
+++ mm-sbdemo-crud/src/main/java/com/sbdemo/crud/MmSbdemoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.sbdemo.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmSbdemoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmSbdemoCrudApplication.class, args);
+	}
+
+}
---
+++ mm-sbdemo-crud/src/test/java/com/sbdemo/crud/MmSbdemoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbdemo.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmSbdemoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

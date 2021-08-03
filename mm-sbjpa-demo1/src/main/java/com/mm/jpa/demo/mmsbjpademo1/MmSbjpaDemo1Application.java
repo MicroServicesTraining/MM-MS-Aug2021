38f5c494daaa9550e2e014f11531e11234e87eb7
sekharbabu4patnam <babuthetrainer@gmail.com>
2021-08-03 06:53:48 +0530

    commit

---
+++ mm-sbjpa-demo1/src/main/java/com/mm/jpa/demo/mmsbjpademo1/MmSbjpaDemo1Application.java
@@ -0,0 +1,13 @@
+package com.mm.jpa.demo.mmsbjpademo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmSbjpaDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmSbjpaDemo1Application.class, args);
+	}
+
+}
---
+++ mm-sbjpa-demo1/src/test/java/com/mm/jpa/demo/mmsbjpademo1/MmSbjpaDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mm.jpa.demo.mmsbjpademo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmSbjpaDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

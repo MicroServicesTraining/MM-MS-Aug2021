77a28fb68e3ac1801280a3de5e6314683f05d482
sekharbabu4patnam <babuthetrainer@gmail.com>
2021-08-13 07:00:23 +0530

    Spring Security Demos

---
+++ mm-cust-sec/src/main/java/com/mm/sec/demo/controller/ProductsController.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.mm.sec.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author USER
+ *
+ */
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+	
+	@GetMapping
+	public String showWelcomePage() {
+		return "<h1>Welcome to Products Service</h1>";
+	}
+	
+}

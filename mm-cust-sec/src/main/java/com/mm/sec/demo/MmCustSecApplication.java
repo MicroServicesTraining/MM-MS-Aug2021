06782acf41dddbf9c603b19399253d7bd1b8b80e
sekharbabu4patnam <babuthetrainer@gmail.com>
2021-08-13 07:01:05 +0530

    Spring Security Demos

---
+++ mm-cust-sec/src/main/java/com/mm/sec/demo/MmCustSecApplication.java
@@ -0,0 +1,13 @@
+package com.mm.sec.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmCustSecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmCustSecApplication.class, args);
+	}
+
+}
